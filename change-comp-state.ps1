268ba2a0d3461d1d031051da4584f04a1b36d933
jasonswalton <132293386+jasonswalton@users.noreply.github.com>
2023-09-22 11:24:51 -0700

    change-comp-state.ps1

--- change-comp-state.ps1
+++ change-comp-state.ps1
@@ -1,4 +1,5 @@
 # Changing computer state
+# https://learn.microsoft.com/en-us/powershell/scripting/samples/changing-computer-state?view=powershell-7.3
 
 # Locking a computer
 rundll32.exe user32.dll,LockWorkStation
